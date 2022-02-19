d733da753898d4dffa4a63a08276f74981956dcb
yunfeiluo <2630064129@qq.com>
2022-02-18 22:16:21 -0500

    update multi-loss

--- src/models.py
+++ src/models.py
@@ -27,19 +27,32 @@ def LongFormer(MODEL_NAME="allenai/longformer-base-4096", MAX_LEN=1024, LR=1e-4)
     # downstream output layer(s)
     out = backbone(input_ids, attention_mask=mask)
     out = tf.keras.layers.Dense(256, activation='relu')(out[0])
-    out1 = tf.keras.layers.Dense(15, activation='softmax', dtype='float32')(out)
-    out2 = tf.keras.layers.Dense(15, activation='softmax', dtype='float32')(out)
-    output = [out1, out2]
+    out1 = tf.keras.layers.Dense(15, activation='softmax', dtype='float32', name="main_task")(out)
+    out2 = tf.keras.layers.Dense(15, activation='softmax', dtype='float32', name="sub_task_0")(out)
+    outputs = [out1, out2]
+    
     
     # define loss
-    loss1 = lambda x, y: 1.0 * tf.keras.losses.CategoricalCrossentropy()(x, y)
-    loss2 = lambda x, y: 1.0 * tf.keras.losses.CategoricalCrossentropy()(x, y)
-    loss = [loss1, loss2]
+    loss = {
+        "main_task": tf.keras.losses.CategoricalCrossentropy(),
+        "sub_task_0": tf.keras.losses.CategoricalCrossentropy()
+    }
+    loss_weights =  {
+        "main_task": 0.8,
+        "sub_task_0": 1.0
+    }
+    
+    mets =  {
+        "main_task": tf.keras.metrics.CategoricalAccuracy(),
+        "sub_task_0": tf.keras.metrics.CategoricalAccuracy()
+    }
     
     # integration
-    model = tf.keras.Model(inputs=[input_ids,mask], outputs=output)
+    model = tf.keras.Model(inputs=[input_ids,mask], outputs=outputs)
     model.compile(optimizer = tf.keras.optimizers.Adam(learning_rate = LR),
                   loss = loss,
-                  metrics = [tf.keras.metrics.CategoricalAccuracy()])
+                  metrics = mets,
+                  loss_weights = loss_weights
+                 )
     
     return model
